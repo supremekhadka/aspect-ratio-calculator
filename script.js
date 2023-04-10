ee80a3b693eeb83acfea77412ae6e36444cfce15
Supreme <supremekhadka2@gmail.com>
2023-04-10 15:43:25 +0545

    Added script in html, updated stylesheet

--- script.js
+++
@@ -1,8 +0,0 @@
-alert("Site Under Construction");
-
-const num1 = document.getElementById("num1");
-const num2 = document.getElementById("num2");
-const ar1 = document.getElementById("ar1");
-const ar2 = document.getElementById("ar2");
-
-let num1_val, num2_val, ar1_val, ar2_val;
